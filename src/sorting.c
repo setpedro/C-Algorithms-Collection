d07a9e9e96e86078a2f7f354bf594013500fe6b0
setPedro <pedross4789@gmail.com>
2025-03-10 23:08:15 +0100

    insertion_sort

--- src/sorting.c
+++ src/sorting.c
@@ -125,3 +125,17 @@ void quick_sort(int array[], int start, int end) {
   quick_sort(array, start, new_pivot_idx - 1);
   quick_sort(array, new_pivot_idx + 1, end);
 }
+
+void insertion_sort(int array[], int size) {
+  for (int i = 1; i < size; i++) {
+    int key = array[i];
+    int j = i - 1;
+
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = key;
+  }
+}
